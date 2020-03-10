c79eace4537d927d67690cd991254a95d241bae6
Alex Mulholland <rc0038722@student.riversidecollege.ac.uk>
2020-03-10 12:41:39 +0000

    Add project files.

---
+++ Volumes/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Volumes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Declare radius value
+            double Radius;
+
+            //Read value from textbox
+            try
+            {
+                Radius = double.Parse(textBox1.Text);
+            }
+
+            catch
+            {
+                MessageBox.Show("Type a number in the radius box");
+                Radius = 0.0;
+            }
+
+            //Calculate volume
+            double Volume = 4.0 / 3.0 * Math.PI * Radius * Radius * Radius;
+            
+            //Dsiplay volume
+            label1.Text = " Volume = " + Volume + "m^3.";
+        }
+    }
+}
+
+  
+
+    
+
